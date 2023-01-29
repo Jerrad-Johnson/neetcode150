050f970516056580a8bbc243374868f44d6b5efd
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2023-01-29 00:46:13 -0700

    Finished dailyTemperatures. Finished search.

--- js/sections/stack.js
+++ js/sections/stack.js
@@ -12,40 +12,44 @@ function stacks(){
     evalRPM.calculateRPN(["2","1","+","3","*"]);
     //cc(evalRPM)
 
-    generateParentheses(3);
+    /*let tempStack = new MinStack();
+    tempStack.createStack([73,74,75,71,69,72,76,73]);*/
+    /*cc(dailyTemperatures([73,74,75,71,69,72,76,73]));
+
+    generateParentheses(3);*/
 }
 
 function isValid(str){
-        let arr = [...str];
-        let tempArr = [];
-        let length = arr.length;
-        let x;
-        let failure = false;
+    let arr = [...str];
+    let tempArr = [];
+    let length = arr.length;
+    let x;
+    let failure = false;
 
-        if (length == 1){
-            return false;
+    if (length == 1){
+        return false;
+    }
+
+    for (i = 0; i < length; i++){
+        x = arr.shift();
+        if (x == "[" || x == "(" || x == "{"){
+            tempArr.push(x);
+        } else if ( (x == ")") && (tempArr.at(-1) == "(") ) {
+            tempArr.splice(-1,1);
+        } else if ( (x == "]") && (tempArr.at(-1) == "[") ) {
+            tempArr.splice(-1,1);
+        } else if ( (x == "}") && (tempArr.at(-1) == "{") ) {
+            tempArr.splice(-1,1);
+        } else {
+            failure = true;
         }
+    }
 
-        for (i = 0; i < length; i++){
-            x = arr.shift();
-            if (x == "[" || x == "(" || x == "{"){
-                tempArr.push(x);
-            } else if ( (x == ")") && (tempArr.at(-1) == "(") ) {
-                tempArr.splice(-1,1);
-            } else if ( (x == "]") && (tempArr.at(-1) == "[") ) {
-                tempArr.splice(-1,1);
-            } else if ( (x == "}") && (tempArr.at(-1) == "{") ) {
-                tempArr.splice(-1,1);
-            } else {
-                failure = true;
-            }
-        }
+    if (tempArr.length != 0){
+        return false;
+    }
 
-        if (tempArr.length != 0){
-            return false;
-        }
-
-        return !failure;
+    return !failure;
 }
 
 function generateParentheses(n = 3){
@@ -75,4 +79,18 @@ function generateParentheses(n = 3){
     return res;
 }
 
+function dailyTemperatures(temperatures){ // Pretty much a copy of https://leetcode.com/problems/daily-temperatures/solutions/1450496/Javascript-easyandclean-solution/
+    let results = new Array(temperatures.length).fill(0);
+    let stack = [];
+
+    for (let iteration = 0; iteration < temperatures.length; iteration++){
+        while (stack.length > 0 && temperatures[iteration] > temperatures[stack[stack.length-1]]){
+            let immediateIndex = stack.pop();
+            results[immediateIndex] = iteration - immediateIndex;
+        }
+        stack.push(iteration);
+    }
+
+    return results;
+}
 
