5e83b1863e4ebff3be151587c47c6af348a75bb6
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2023-02-03 00:45:35 -0700

    Added a BST class.

---
+++ js/sections/trees.js
@@ -0,0 +1,54 @@
+// based on https://www.youtube.com/watch?v=5cU1ILGy6dM&t=120s
+class Node{
+    constructor(data, left = null, right = null){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BST{
+    constructor(){
+        this.root = null;
+    }
+
+    add(data){
+        const node = this.root;
+        if (node === null){
+            this.root = new Node(data);
+            return;
+        } else {
+            this.searchTree(node, data);
+        }
+    }
+
+    addArr(arr){
+        for (let entry of arr){
+            this.add(entry);
+        }
+    }
+
+    searchTree(node, data){
+        if (data < node.data){
+            if (node.left === null){
+                node.left = new Node(data);
+                return;
+            } else {
+                return this.searchTree(node.left, data);
+            }
+        } else if (data > node.data){
+            if (node.right === null){
+                node.right = new Node(data);
+                return;
+            } else {
+                return this.searchTree(node.right, data);
+            }
+        } else {
+            return null;
+        }
+    }
+}
+
+let tree = new BST;
+tree.addArr([1,5,2,7,4,1,6,8,32,2,15,5,0]);
+cc(tree);
