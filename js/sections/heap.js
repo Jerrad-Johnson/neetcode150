2f90c1cbe2c2d3511c0c5117a33387fe13b1ab33
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2023-02-04 21:42:25 -0700

    Finished lastStoneWeight

--- js/sections/heap.js
+++ js/sections/heap.js
@@ -1,5 +1,5 @@
 class MinHeap{
-    constructor(k){
+    constructor(k = Number.POSITIVE_INFINITY){
         this.heap = [];
         this.k = k
     }
@@ -24,9 +24,34 @@ class MinHeap{
     getKth(){
         return this.heap[0];
     }
+
+    lastStoneWeight(){
+        let limiter = 0;
+        while (this.heap.length > 1){
+            limiter++; if (limiter === 50) return;
+            this.heap.sort();
+            let length = this.heap.length;
+            let remainder = this.heap[length-1] - this.heap[length-2];
+            if (remainder !== 0){
+                this.heap.pop();
+                this.heap.pop();
+                this.heap.unshift(remainder);
+            } else {
+                this.heap.pop();
+                this.heap.pop();
+            }
+        }
+
+        return this.heap;
+    }
 }
 
 let x = new MinHeap(3);
 x.add([1,2,3])
 x.add(5);
-cc(x.getKth());
+//cc(x.getKth());
+
+let stones = new MinHeap();
+stones.add([2,7,4,1,8,1]);
+cc(stones.lastStoneWeight());
+
