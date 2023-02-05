5cfb90dcab87918bbfc816b69b0aece0e4bce152
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2023-02-05 13:00:40 -0700

    Finished subsets

---
+++ js/sections/backtracking.js
@@ -0,0 +1,17 @@
+// From https://medium.com/@alexanderpavlov_18884/javascript-algorithms-backtracking-222cde11842c
+function subsets(nums) {
+    let combinations = [];
+
+    function backtrack(nums, path){
+        combinations.push([...path]);
+        for (var i = 0; i < nums.length; i++){
+            path.push(nums[i]);
+            backtrack(nums.slice(i + 1), path);
+            path.pop();
+        }
+    }
+    backtrack(nums, []);
+    return combinations;
+};
+
+cc(subsets([1,5,2]))
