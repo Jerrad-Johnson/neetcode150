28ff78551c2f4680fc2a4e1e4e5df10053940911
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2023-02-05 13:35:20 -0700

    Beginning graphs.js

--- js/sections/backtracking.js
+++ js/sections/backtracking.js
@@ -12,6 +12,23 @@ function subsets(nums) {
     }
     backtrack(nums, []);
     return combinations;
-};
+}
 
-cc(subsets([1,5,2]))
+function combinationSum(){
+    let combinations = [];
+    let sums = [];
+
+    function backtrack(nums, path){
+        combinations.push([...path]);
+        if (path.reduce((a, b) => a + b) === target) sums.push([...path]);
+        for (var i = 0; i < nums.length; i++){
+            path.push(nums[i]);
+            backtrack(nums.slice(i + 1), path);
+            path.pop();
+        }
+    }
+    backtrack(nums, []);
+    return combinations;
+}
+
+//cc(subsets([1,5,2]));
