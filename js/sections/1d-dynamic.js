a9fed8491efda9b936d2e744b68e2bd6ceb3eaf8
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2023-02-10 20:46:10 -0700

    Finished minCostClimbingStairs

--- js/sections/1d-dynamic.js
+++ js/sections/1d-dynamic.js
@@ -11,4 +11,24 @@ function climbStairs(n){
     return curr;
 }
 
-cc(climbStairs(12))
+function minCostClimbingStairs(arr){ // Basically works, but I slightly misunderstood the requirement. It *should* also count the last step.
+    let min = {min: Number.POSITIVE_INFINITY};
+
+    recursion(0); recursion(1);
+
+    function recursion(i, total = 0){
+        if (i !== arr.length-1) total = total + arr[i];
+        if (i === arr.length-1 && total < min.min) {min.min = total; cc("IRAN")};
+
+        if (i < arr.length){
+            recursion(i+1, total);
+            recursion(i+2, total);
+        }
+    }
+
+    return min;
+}
+
+//cc(climbStairs(12))
+ cc(minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1])) // Correct
+// cc(minCostClimbingStairs([10,15,20]))
